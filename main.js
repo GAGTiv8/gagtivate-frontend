781a7a2cd3168e6257d27a1f6c55a257647e121d
edycakra <edycakrasinaga@gmail.com>
2020-02-07 13:19:33 +0700

    minor update

--- main.js
+++ main.js
@@ -1,97 +1,118 @@
-const baseUrl = `http://localhost:3000`
+// const baseUrl = `http://localhost:3000`
 
 function uploadImage() {
-    const titleImage = $('#uploadTitle')
-    const urlImage = $('#uploadImage')
-    const tagImage = $('#uploadTag')
+    // e.preventDefault()
+    const newImage = $('#uploadTitle')
+    const newUrl = $('#uploadImage')
+    const newTag = $('#uploadTag')
+    console.log('masuk uploadImage')
+    console.log(newImage)
+    // const config = {
+    //     headers: { 
+    //         access_token: localStorage.token
+    //     }
+    // }
+    // axios.post('http://localhost:3000/posts', config)
 
     $.ajax({
-        url: `${baseUrl}/posts`,
+        url: `http://localhost:3000/posts`,
         method: `POST`,
         data: {
-            titleImage, urlImage, tagImage
+            title: newImage, 
+            url: newUrl, 
+            tags: newTag
         },
         headers: {
-            access_token: localStorage.access_token
+            access_token: localStorage.token
         }
     })
     .done(response => {
         console.log(`upload success!`)
-        fetchImage()
-    })
-    .fail(err => {
-        console.log(err)
-    })
-}
-
-function fetchAll() {
-    $.ajax(`${baseUrl}/posts`, {
-        method: 'GET',
-        headers: {
-            access_token: localStorage.access_token
-        }
-    })
-    .done(images => {
-        //belum tau format hasil images nya, coba console log dulu biar ga kegulung
-        console.log(images)
-        $('#title-image').empty()
-    })
-    .fail(err => {
-        console.log(err)
-    })
-}
-
-function fetchOne(id) {
-    $.ajax(`${baseUrl}/posts/${id}`, {
-        method: 'GET',
-        headers: {
-            access_token: localStorage.access_token
-        }
-    })
-    .done(image => {
-        //mau cek isi result image dulu
-        console.log(image)
-    })
-    .fail(err => {
-        console.log(err)
-    })
-}
-
-function updateImage(id) {
-    const editTitle = $('#editTitle')
-    const editTag = $('#editTag')
-
-    $.ajax(`${baseUrl}/posts/${id}`, {
-        method: 'PUT',
-        data: {
-            editTitle, editTag
-        },
-        headers: {
-            access_token: localStorage.access_token
-        }
-    })
-    .done(image => {
-        console.log(image)
-    })
-    .fail(err => {
-        console.log(err)
-    })
-}
-
-function deleteImage(id) {
-    $.ajax({
-        url: `${baseUrl}/posts/${+id}`,
-        method: 'DELETE',
-        headers: {access_token: localStorage.access_token}
-    })
-    .done((data) => {
-        console.log(`post with ${id} deleted!`)
         fetchAll()
     })
-    .fail((err) => {
-        console.log(err)
+    .fail(err => {
+        console.log(`upload error`)
     })
 }
+
+// function fetchAll() {
+//     $.ajax(`http://localhost:3000/posts`, {
+//         method: 'GET',
+//         headers: {
+//             access_token: localStorage.access_token
+//         }
+//     })
+//     .done(images => {
+//         //belum tau format hasil images nya, coba console log dulu biar ga kegulung
+//         console.log(images.data)
+//         let newArr = images.data 
+//         $('#image-dashboard').empty()
+//         for (let i in newArr) {
+//             $('#image-dashboard').append(`
+//             <div class="w3-container w3-center">
+//             <p>${newArr[i].title}</p>
+//           </div>
+//           <img src=${newArr[i].url} style="width:100%">
+//           <center><p>Tag: ${newArr[i].tags}| <a href="#" onclick="deleteImage()">Delete</a></p></center>
+//             `)
+//         }
+//     })
+//     .fail(err => {
+//         console.log(err)
+//     })
+// }
+
+// function fetchOne(id) {
+//     $.ajax(`http://localhost:3000/posts/${id}`, {
+//         method: 'GET',
+//         headers: {
+//             access_token: localStorage.access_token
+//         }
+//     })
+//     .done(image => {
+//         //mau cek isi result image dulu
+//         console.log(image)
+//     })
+//     .fail(err => {
+//         console.log(err)
+//     })
+// }
+
+// function updateImage(id) {
+//     const editTitle = $('#editTitle')
+//     const editTag = $('#editTag')
+
+//     $.ajax(`http://localhost:3000/posts/${id}`, {
+//         method: 'PUT',
+//         data: {
+//             editTitle, editTag
+//         },
+//         headers: {
+//             access_token: localStorage.access_token
+//         }
+//     })
+//     .done(image => {
+//         console.log(image)
+//     })
+//     .fail(err => {
+//         console.log(err)
+//     })
+// }
+
+// function deleteImage(id) {
+//     $.ajax({
+//         url: `http://localhost:3000/posts/${+id}`,
+//         method: 'DELETE',
+//         headers: {access_token: localStorage.access_token}
+//     })
+//     .done((data) => {
+//         console.log(`post with ${id} deleted!`)
+//         fetchAll()
+//     })
+//     .fail((err) => {
+//         console.log(err)
+//     })
+// }
 
 // // OPTIONAL
 // function fetchByTag(tag) {
@@ -100,6 +121,7 @@ function deleteImage(id) {
     //findOneImage by specific Tag
     //updateImage by Id
     //deleteImage by Id
+
 function showLoginButton() {
     $(".g-signin2").show()
     $("button#logout").hide()
@@ -121,8 +143,15 @@ function initialInterface() {
 $(document).ready(function() {
     // initialInterface()
     //upload image
-    $('#uploadAll').on("submit", (e) => {
-        e.preventDefault()
+    // $('#uploadAll').on("submit", (e) => {
+    //     e.preventDefault()
+    //     console.log('hehehhe')
+    //     uploadImage()
+    // })
+
+    $("#addImage").submit(function( event ) {
+        event.preventDefault()
+        console.log('cki')
         uploadImage()
     })
 
